a3c94e6341e484a03423c753df2b8a19d5d963d6
Cheikh Diouf <cadiouf@orbusdigital.com>
2026-02-11 10:33:54 +0000

    first commit

---
+++ tpDevSecOps/src/main/java/com/example/tpDevSecOps/TpDevSecOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpDevSecOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDevSecOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDevSecOpsApplication.class, args);
+	}
+
+}
---
+++ tpDevSecOps/src/test/java/com/example/tpDevSecOps/TpDevSecOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpDevSecOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpDevSecOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
