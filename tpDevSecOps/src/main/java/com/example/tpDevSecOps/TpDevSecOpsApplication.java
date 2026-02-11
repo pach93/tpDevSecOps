730944e5baf9ca21bab8d1f0b68a4ffd038b68a9
Cheikh Diouf <cadiouf@orbusdigital.com>
2026-02-11 10:38:23 +0000

    updates

--- tpDevSecOps/src/main/java/com/example/tpDevSecOps/TpDevSecOpsApplication.java
+++ tpDevSecOps/src/main/java/com/example/tpDevSecOps/TpDevSecOpsApplication.java
@@ -2,7 +2,7 @@ package com.example.tpDevSecOps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 public class TpDevSecOpsApplication {
 
